7ac7ff1fb560604f0d16b84f7f9432539f591485
Cody Miller <cdymilr@gmail.com>
2022-02-23 23:31:17 -0700

    Gitlab commit

--- DevOpsInt.java
+++ DevOpsInt.java
@@ -1,7 +1,7 @@
 
 public class DevOpsInt{
     public static void main(String[] args){
-        System.out.println("Hello DevOps Class!");
+        System.out.println("Hello DevOps Class from Gitlab!");
     }
 
 }
